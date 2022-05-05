19f67416bb13ca3deb691f9cb9f509bc193d01ba
dshaby <daniel@businessesoftheearth.org>
2022-05-04 19:09:44 -0700

    React

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,8 +1,67 @@
+import { useCallback, useReducer } from "react";
+import Input from "../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import "./NewPlace.css";
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {};
+    default:
+      return state;
+  }
+};
+
 const NewPlace = () => {
+  useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
-    <div>
-      <h1>places/new works!</h1>
-    </div>
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (of at least 5 characters)"
+        onInput={descriptionInputHandler}
+      />
+    </form>
   );
 };
 
---
+++ src/shared/components/UIElements/Map.jsx
@@ -0,0 +1,32 @@
+import "./Map.css";
+import { useEffect, useRef } from "react";
+const Map = (props) => {
+  //   const map = new google.maps.Map(document.getElementById("map"), {
+  //       center:
+  //   })
+
+  const mapRef = useRef();
+
+  const { center, zoom } = props;
+
+  useEffect(() => {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: center,
+      zoom: zoom,
+    });
+    new window.google.maps.Marker({ position: props.center, map: map });
+    // eslint-disable-next-line
+  }, [center, zoom]);
+
+  return (
+    <>
+      <div
+        ref={mapRef}
+        className={`map ${props.className}`}
+        style={props.style}
+      ></div>
+    </>
+  );
+};
+
+export default Map;
